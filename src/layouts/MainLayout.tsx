703b85d18a8e928024b1ad2267936bf042b7d382
gsofter <real.kingstar@outlook.com>
2021-08-22 12:13:17 -0500

    update form

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,14 +1,30 @@
 import React from "react";
-import { Layout } from "antd";
+import { Layout, Button } from "antd";
+import { useSafeAppsSDK } from "@gnosis.pm/safe-apps-react-sdk";
 import "./MainLayout.css";
 
 const { Header, Content, Footer } = Layout;
 
 export const MainLayout: React.FC = ({ children }) => {
+  const { sdk, connected, safe } = useSafeAppsSDK();
+  console.log(safe.safeAddress);
   return (
     <Layout>
-      <Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
-        <h3 style={{ color: "wheat" }}> OpenSea Auction </h3>
+      <Header
+        style={{
+          position: "fixed",
+          zIndex: 1,
+          width: "100%",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <h3 style={{ color: "wheat", float: "left" }}> OpenSea Auction </h3>
+        <Button type="primary" style={{ float: "right" }}>
+          {" "}
+          Connect Wallet{" "}
+        </Button>
       </Header>
       <Content
         className="site-layout"
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,63 @@
 import React from "react";
+import { Form, Input, Button, Row, Col } from "antd";
+
+const BidForm: React.FC = () => {
+  const onFinish = (values: any) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <Form
+      name="basic"
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 16 }}
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+      <Form.Item
+        label="OpenSea URL"
+        name="bidUrl"
+        rules={[{ required: true, message: "Please input your opensea url!" }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Safe address"
+        name="safeAddress"
+        initialValue={"0x22dafd3232"}
+      >
+        <Input disabled />
+      </Form.Item>
+
+      <Form.Item
+        label="TotalBalance"
+        name="totalBalance"
+        initialValue={"9 ETH"}
+      >
+        <Input disabled />
+      </Form.Item>
+
+      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Button type="primary" htmlType="submit">
+          Make Offer
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
 
 export const MainPage: React.FC = () => {
-  return <></>;
+  return (
+    <Row justify="start">
+      <Col xs={24} md={18} lg={12}>
+        <BidForm />
+      </Col>
+    </Row>
+  );
 };
